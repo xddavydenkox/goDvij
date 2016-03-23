79039257ea01d51a49e8acf8bbb9d7856904eba8
xddavydenkox <ddavydenko@plslogistics.com>
2016-03-23 18:26:43 +0200

    Added navigation bar without functionality, added timer. Set background. Set Bottom Line.

--- js/app/app.js
+++ js/app/app.js
@@ -3,6 +3,5 @@
 angular.module('goDvijApp', ['ngResource','ngRoute']).config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'templates/pages/home.html', controller: 'HomePageCtrl'});
-      
     }
 ]);
--- js/app/controllers/homePageCtrl.js
+++ js/app/controllers/homePageCtrl.js
@@ -1,7 +1,34 @@
 'use strict';
 
-angular.module('goDvijApp').controller('HomePageCtrl', ['$scope',
-	function($scope) {
+angular.module('goDvijApp').controller('HomePageCtrl', ['$scope', '$interval',
+	function($scope, $interval) {
 
+		$scope.timeForEvent = {};
+
+		$interval(function() {
+			var t = getTimeRemaining("2017-02-02");
+				$scope.timeForEvent = {
+				   	total: t.total,
+					days: t.days,
+				   	hours: t.hours,
+				   	minutes: t.minutes,
+				   	seconds: t.seconds
+				   }
+		},1000)
+
+		function getTimeRemaining(endtime) {
+			var t = Date.parse(endtime) - Date.parse(new Date());
+			var seconds = Math.floor( (t/1000) % 60 );
+			var minutes = Math.floor( (t/1000/60) % 60 );
+			var hours = Math.floor( (t/(1000*60*60)) % 24 );
+			var days = Math.floor( t/(1000*60*60*24) );
+			return {
+			   	'total': t,
+			   	'days': days,
+			   	'hours': hours,
+			   	'minutes': minutes,
+			   	'seconds': seconds
+		  	};	
+		}
 	}
 ]);
